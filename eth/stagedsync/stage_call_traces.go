81a042d66c38440e861ab7e0dd73cb98efb7110c
Artem Vorotnikov <artem@vorotnikov.me>
2021-04-19 21:46:21 +0300

    wip

--- eth/stagedsync/stage_call_traces.go
+++ eth/stagedsync/stage_call_traces.go
@@ -132,7 +132,7 @@ func promoteCallTraces(logPrefix string, tx ethdb.Database, startBlock, endBlock
 			return err
 		}
 		if block == nil {
-			break
+			return fmt.Errorf("no block: %d", blockNum)
 		}
 
 		var stateReader state.StateReader
@@ -257,7 +257,7 @@ func unwindCallTraces(logPrefix string, db ethdb.Database, from, to uint64, chai
 
 	tracer := NewCallTracer()
 	vmConfig := &vm.Config{Debug: true, NoReceipts: true, Tracer: tracer}
-	for blockNum := to + 1; blockNum <= from; blockNum++ {
+	for blockNum := from; blockNum > to; blockNum-- {
 		if err := common.Stopped(quitCh); err != nil {
 			return err
 		}
@@ -271,7 +271,7 @@ func unwindCallTraces(logPrefix string, db ethdb.Database, from, to uint64, chai
 			return err
 		}
 		if block == nil {
-			break
+			return fmt.Errorf("no block %d", blockNum)
 		}
 
 		stateReader := state.NewPlainDBState(db, blockNum-1)
